cc98e6b1b0d3b1e4233c4ec76ff31b8f5beaa18d
unknown <150742495+Tinxte@users.noreply.github.com>
2024-03-17 21:24:25 -0500

    5 out of 9

---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+function Banner() {
+    return(
+        <h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </h1>
+    )
+}
+
+export default Banner;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,36 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
+  return(
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
+      
+      {/* //first button */}
+
+      {displaySats.map((sat, id) => {
+        return (
+        <button onClick={() => filterByType(sat)} key ={id}>
+          {sat} Orbit
+        </button>
+      );
+    })}
+
+      {/* //second button: all orbits */}
+
+        {/* {return ( */}
+          <button onClick={() => setSat(satData)}>
+            All Orbits
+          </button>
+        {/* )} */}
+
+      {/* return (
+        <div>
+          <button>Placeholder Button</button>
+          <button>All Orbits</button>
+        </div>
+      ); */}
+      
+      </div>
+);
 };
 
 export default Buttons;
